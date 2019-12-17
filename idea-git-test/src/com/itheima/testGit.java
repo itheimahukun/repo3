de2c65e92a510fe4c1347bc41d0aa8a7e4c650c3
zhangsan <zhangsan1@itcast.com>
2019-12-17 18:30:52 +0800

    新建并提交本地工程

---
+++ idea-git-test/src/com/itheima/testGit.java
@@ -0,0 +1,13 @@
+package com.itheima;
+
+public class testGit {
+
+    public static void main(String[] args) {
+        System.out.println("nananana");
+        System.out.println("nananana");
+        System.out.println("nananana");
+        System.out.println("nananana");
+        System.out.println("nananana");
+        System.out.println("nananana");
+    }
+}
